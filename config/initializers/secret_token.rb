1a10a9bda0d4643f0e18de2599f957cf37fd46da
Surakarn Samkaew <tonkla@gmail.com>
2011-02-15 18:56:15 +0700

    Start the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bilan::Application.config.secret_token = 'd80060215020e3dfa1ea15b5c0689ecaf0c1adb4cdba38a6bf14502ebb031f8d7fff55af03299e361dc890ab02726ee510b5825a21bfb5886b5d350ec30dcada'
