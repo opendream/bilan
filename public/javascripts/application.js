1b3571dae7878658b3623d8ae785988f28da07b8
Surakarn Samkaew <tonkla@gmail.com>
2011-03-04 20:38:15 +0700

    Add to the project

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,29 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+    $('select#publication_publication_type').change(function() {
+        if (this.value == 9) {
+            $('p.publication_type_other').show('normal');
+            $('#publication_publication_type_other').focus();
+        } else {
+            $('p.publication_type_other').hide('normal');
+            $('#publication_publication_type_other').val('');
+        }
+    });
+
+    $('input#publication_translated_false').click(function() {
+        $('p.translated').each(function() {
+            $(this).hide('normal');
+            $('#publication_translated_from').val('');
+            $('#publication_original_author').val('');
+        });
+    });
+
+    $('input#publication_translated_true').click(function() {
+        $('p.translated').each(function() {
+            $(this).show('normal');
+            $('#publication_translated_from').focus();
+        });
+    });
+
+    $('fieldset.collapsible').collapse();
+    $('textarea.resizable:not(.processed)').TextAreaResizer();
+});
