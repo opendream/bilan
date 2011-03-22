df8a68e5833b3c00c37b1842a3c0ae834354384e
Surakarn Samkaew <tonkla@gmail.com>
2011-03-22 20:21:47 +0700

    Show more useful information on Dashboard

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,16 +1,17 @@
 class UserController < ApplicationController
 
   before_filter :authenticate_user!
-  before_filter :set_active_tab
 
   def dashboard
     @publishers = Publisher.where(:owner_id => current_user).order('created_at')
+    @isbn_queue = Publication.where("(isbn10 IS NULL AND isbn13 IS NULL) AND publisher_id IN (?)",
+      current_user.publisher_ids)
+    @cip_queue = Publication.where("cip = '' AND (isbn10 IS NOT NULL OR isbn13 IS NOT NULL) "+
+      "AND publisher_id IN (?)", current_user.publisher_ids)
+    @completed_publications = Publication.where("(isbn10 IS NOT NULL OR isbn13 IS NOT NULL) AND cip <> ''" +
+      "AND publisher_id IN (?)", current_user.publisher_ids).order('created_at DESC').limit(15)
   end
 
   private
-
-  def set_active_tab
-    @active_tab = 'dashboard'
-  end
 
 end
